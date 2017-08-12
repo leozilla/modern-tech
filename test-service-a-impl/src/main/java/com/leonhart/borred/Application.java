992fb363574b9132197fcb340f4d99184c4f07b6
David Leonhart <leozilla@gmail.com>
2017-08-12 09:54:02 +0200

    Added vertx

--- test-service-a-impl/src/main/java/com/leonhart/borred/Application.java
+++ test-service-a-impl/src/main/java/com/leonhart/borred/Application.java
@@ -1,5 +1,8 @@
 package com.leonhart.borred;
 
+import io.vertx.core.Vertx;
+import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.core.http.HttpServerResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +11,17 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
+import static io.vertx.core.Vertx.*;
+
 @SpringBootApplication
 public class Application {
 
-   public static void main(String[] args) {
+   public static void main(final String[] args) {
       SpringApplication.run(Application.class, args);
    }
 
    @Bean
-   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+   public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
       return args -> {
       };
    }
---
+++ test-service-a-impl/src/main/java/com/leonhart/borred/vehicles/HttpServerVehicle.java
@@ -0,0 +1,31 @@
+package com.leonhart.borred.vehicles;
+
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.core.http.HttpServerResponse;
+
+import static io.vertx.core.Vertx.vertx;
+
+public class HttpServerVehicle extends AbstractVerticle {
+
+   @Override
+   public void start() throws Exception {
+      HttpServer server = vertx.createHttpServer();
+      server.requestStream().toObservable().subscribe(req -> {
+         HttpServerResponse resp = req.response();
+
+         String contentType = req.getHeader("Content-Type");
+         if (contentType != null) {
+            resp.putHeader("Content-Type", contentType);
+         }
+
+         resp.setChunked(true);
+         req.toObservable().subscribe(
+                 buffer -> resp.write("Hello Vertx", "UTF-8"),
+                 err -> {},
+                 resp::end
+         );
+      });
+      server.listen(8081);
+   }
+}
